c3092af84149dda0915bac5bcac674e53e19e3c4
Conrad Ratschan <conrad.ratschan@gmail.com>
2020-05-22 00:17:56 -0500

    common: Switch to use ggez instead of sdl2

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,16 +1,15 @@
-
-struct Cpu {
+pub struct Cpu{
     registers: Registers,
     memory: [u8; 4096],
 }
 
 struct Registers {
-    v: [u8; 16],     // Vx where x is a hexadecimal digit 0..F
-    i: u16,          // Generally used to store memory addresses
+    v: [u8; 16], // Vx where x is a hexadecimal digit 0..F
+    i: u16,      // Generally used to store memory addresses
     delay_timer: u8,
     sound_timer: u8,
-    pc: u16,         // Program Counter - used to store currently executing address
-    sp: u8,          // Stack Pointer - used to point to the topmost level of the stack
+    pc: u16, // Program Counter - used to store currently executing address
+    sp: u8,  // Stack Pointer - used to point to the topmost level of the stack
     stack: [u16; 16],
 }
 
@@ -29,14 +28,14 @@ impl Registers {
 }
 
 impl Cpu {
-    fn initialize() -> Cpu {
+    pub fn initialize() -> Cpu {
         Cpu {
             registers: Registers::initialize(),
             memory: [0; 4096],
         }
     }
 
-    fn process_opcode(&mut self, opcode: u16) {
+    pub fn process_opcode(&mut self, opcode: u16) {
         match opcode >> 12 {
             0x0 => {
                 // 0xxx
@@ -44,7 +43,7 @@ impl Cpu {
                     0x00e0 => {
                         // CLS - Clear display
                         //TODO: unimplemented
-                    },
+                    }
                     0x00ee => {
                         // RET - return from subroutine
                         if self.registers.sp == 0 {
@@ -52,36 +51,36 @@ impl Cpu {
                         }
                         self.registers.pc = self.registers.stack[self.registers.sp as usize];
                         self.registers.sp -= 1;
-                    },
+                    }
                     _ => {
                         // Legacy routine, ignored
                     }
                 }
-            },
+            }
             0x1 => {
                 // 1nnn - JP addr - Jump to location nnn
                 self.registers.pc = opcode & 0xfff;
-            },
+            }
             0x2 => {
                 // 2nnn - CALL addr - Call subroutine at nnn
                 self.registers.sp += 1;
                 self.registers.stack[self.registers.sp as usize] = self.registers.pc;
                 self.registers.pc = opcode & 0xfff;
-            },
+            }
             0x3 => {
                 // 3xkk - SE Vx, byte - Skip next instruction if Vx = kk
                 let x = get_nibble(2, opcode);
                 if self.registers.v[x] == (opcode & 0xff) as u8 {
                     self.registers.pc += 2;
                 }
-            },
+            }
             0x4 => {
                 // 4xkk - SNE Vx, byte - Skip next instruction if Vx != kk
                 let x = get_nibble(2, opcode);
                 if self.registers.v[x] != (opcode & 0xff) as u8 {
                     self.registers.pc += 2;
                 }
-            },
+            }
             0x5 => {
                 // 5xy0 - SE Vx, Vy - Skip next instruction if Vx = Vy
                 let x = get_nibble(2, opcode);
@@ -89,18 +88,18 @@ impl Cpu {
                 if self.registers.v[x] == self.registers.v[y] {
                     self.registers.pc += 2;
                 }
-            },
+            }
             0x6 => {
                 // 6xkk - LD Vx, byte - Set Vx := kk
                 let x = get_nibble(2, opcode);
                 self.registers.v[x] = (opcode & 0xff) as u8;
-            },
+            }
             0x7 => {
                 // 7xkk - ADD Vx, byte - Set Vx := Vx + kk
                 let x = get_nibble(2, opcode);
                 let val = self.registers.v[x] + (opcode & 0xff) as u8;
                 self.registers.v[x] = val;
-            },
+            }
             0x8 => {
                 // 8xyo - Operations between Vx and Vy depending on the value of o
                 let x = get_nibble(2, opcode);
@@ -115,29 +114,29 @@ impl Cpu {
                 if self.registers.v[x] != self.registers.v[y] {
                     self.registers.pc += 2;
                 }
-            },
+            }
             0xa => {
                 // Annn - LD I, addr - Set I := nnn
                 self.registers.i = opcode & 0xfff;
-            },
+            }
             0xb => {
                 // Bnnn - JP V0, addr - Jump to location nnn + V0
                 let loc = opcode & 0xfff;
                 self.registers.pc = loc + self.registers.v[0] as u16;
-            },
+            }
             0xc => {
                 // Cxkk - RND Vx, byte - Set Vx := random byte AND kk
                 let x = get_nibble(2, opcode);
                 let val = opcode & 0xff;
                 //TODO: unimplemented
-            },
+            }
             0xd => {
                 // Dxyn - DRW Vx, Vy, nibble - Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
                 let x = get_nibble(2, opcode);
                 let y = get_nibble(1, opcode);
                 let n = get_nibble(0, opcode);
                 //TODO: unimplemented
-            },
+            }
             0xe => {
                 // Exoo - Key operations with Vx depending on the value of oo
                 let x = get_nibble(2, opcode);
@@ -145,21 +144,21 @@ impl Cpu {
                     0x9e => {
                         // Ex9E - SKP Vx - Skip next instruction if key with the value of Vx is pressed
                         //TODO: unimplemented
-                    },
+                    }
                     0xa1 => {
                         // ExA1 - SKNP Vx - Skip next instruction if key with value of Vx is not pressed
                         //TODO: unimplemented
-                    },
-                    _ => panic!("Invalid opcode: {}", opcode)
+                    }
+                    _ => panic!("Invalid opcode: {}", opcode),
                 }
-            },
+            }
             0xf => {
                 // Fxoo - Operations with Vx depending on the value of oo
                 let x = get_nibble(2, opcode);
                 let op = (opcode & 0xff) as u8;
                 self.process_opcode_f(x, op);
-            },
-            _ => panic!("Unhandled opcode: {}", opcode)
+            }
+            _ => panic!("Unhandled opcode: {}", opcode),
         }
     }
 
@@ -168,19 +167,19 @@ impl Cpu {
             0x0 => {
                 // 8xy0 - LD Vx, Vy - Set Vx := Vy
                 self.registers.v[x] = self.registers.v[y];
-            },
+            }
             0x1 => {
                 // 8xy1 - OR Vx, Vy - Set Vx := Vx OR Vy
-                self.registers.v[x] = self.registers.v[x] | self.registers.v[y];
-            },
+                self.registers.v[x] |= self.registers.v[y];
+            }
             0x2 => {
                 // 8xy2 - AND Vx, Vy - Set Vx := Vx AND Vy
-                self.registers.v[x] = self.registers.v[x] & self.registers.v[y];
-            },
+                self.registers.v[x] &= self.registers.v[y];
+            }
             0x3 => {
                 // 8xy3 - XOR Vx, Vy - Set Vx := Vx XOR Vy
-                self.registers.v[x] = self.registers.v[x] ^ self.registers.v[y];
-            },
+                self.registers.v[x] ^= self.registers.v[y];
+            }
             0x4 => {
                 // 8xy4 - ADD Vx, Vy - Set Vx := Vx + Vy, set VF := carry
                 let (val, carry) = (self.registers.v[x]).overflowing_add(self.registers.v[y]);
@@ -190,7 +189,7 @@ impl Cpu {
                 } else {
                     self.registers.v[0xf] = 0x0;
                 }
-            },
+            }
             0x5 => {
                 // 8xy5 - SUB Vx, Vy - Set Vx := Vx - Vy, set VF := NOT borrow
                 let (val, borrow) = (self.registers.v[x]).overflowing_sub(self.registers.v[y]);
@@ -200,12 +199,12 @@ impl Cpu {
                 } else {
                     self.registers.v[0xf] = 0x0;
                 }
-            },
+            }
             0x6 => {
                 // 8xy6 - SHR Vx - Set Vx := Vx >> 1
                 self.registers.v[0xf] = self.registers.v[x] & 0x1;
-                self.registers.v[x] = self.registers.v[x] >> 1;
-            },
+                self.registers.v[x] >>= 1;
+            }
             0x7 => {
                 // 8xy7 - SUBN Vx, Vy - Set Vx := Vy - Vx, set VF := NOT borrow
                 let (val, borrow) = (self.registers.v[y]).overflowing_sub(self.registers.v[x]);
@@ -215,13 +214,13 @@ impl Cpu {
                 } else {
                     self.registers.v[0xf] = 0x0;
                 }
-            },
+            }
             0xe => {
                 // 8xyE - SHL Vx - Set Vx := Vx << 1
                 self.registers.v[0xf] = self.registers.v[x] & 0x8;
-                self.registers.v[x] = self.registers.v[x] << 1;
-            },
-            _ => panic!("Unhandled opcode: {}", op)
+                self.registers.v[x] <<= 1;
+            }
+            _ => panic!("Unhandled opcode: {}", op),
         }
     }
 
@@ -230,44 +229,44 @@ impl Cpu {
             0x07 => {
                 // Fx07 - LD Vx, DT - Set Vx := Delay timer value
                 self.registers.v[x] = self.registers.delay_timer;
-            },
+            }
             0x0a => {
                 // Fx0A - LD Vx, K - Wait for a key press, store the value of the key in Vx
                 //TODO: unimplemented
-            },
+            }
             0x15 => {
                 // Fx15 - LD DT, Vx - Set delay timer := Vx
                 self.registers.delay_timer = self.registers.v[x];
-            },
+            }
             0x18 => {
                 // Fx18 - LD ST, Vx - Set sound timer := Vx
                 self.registers.sound_timer = self.registers.v[x];
-            },
+            }
             0x1e => {
                 // Fx1E - ADD I, Vx - Set I := I + Vx
-                self.registers.i = self.registers.i + self.registers.v[x] as u16;
-            },
+                self.registers.i += self.registers.v[x] as u16;
+            }
             0x29 => {
                 // Fx29 - LD F, Vx - Set I := location of sprite for digit Vx
                 //TODO: unimplemented
-            },
+            }
             0x33 => {
                 // Fx33 - LD B, Vx - Store BCD representation of Vx in memory locations I, I+1, and I+2
                 //TODO: unimplemented
-            },
+            }
             0x55 => {
                 // Fx55 - LD [I], Vx - Store registers V0 through Vx, in memory starting at location I
                 for i in 0..x {
                     self.memory[self.registers.i as usize + i] = self.registers.v[i];
                 }
-            },
+            }
             0x65 => {
                 // Fx65 - LD Vx, [I] - Read registers V0 through Vx from memory starting at location I
                 for i in 0..x {
                     self.registers.v[i] = self.memory[self.registers.i as usize + i];
                 }
-            },
-            _ => panic!("Unhandled opcode: {}", op)
+            }
+            _ => panic!("Unhandled opcode: {}", op),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,68 @@
-extern crate sdl2;
-
 mod cpu;
 
-use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
-use std::time::Duration;
+extern crate ggez;
 
-fn main() {
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+use ggez::event;
+use ggez::graphics;
+use ggez::{Context, GameResult};
 
-    let window = video_subsystem.window("chip8", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
+const C8_WIDTH: u32 = 640;
+const C8_HEIGHT: u32 = 320;
 
-    let mut canvas = window.into_canvas().build().unwrap();
+struct MainState {
+    pos_x: f32,
+    cpu: cpu::Cpu,
+}
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-
-    'main: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
-                    break 'main
-                },
-                _ => {}
-            }
-        }
-        canvas.present();
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+impl MainState {
+    fn new(ctx: &mut Context) -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0, cpu: cpu::Cpu::initialize() };
+        Ok(s)
     }
+}
+
+impl event::EventHandler for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        graphics::clear(ctx, [0.1, 0.2, 0.3, 1.0].into());
+
+        let circle = graphics::Mesh::new_circle(
+            ctx,
+            graphics::DrawMode::fill(),
+            ggez::nalgebra::Point2::new(0.0, 0.0),
+            100.0,
+            2.0,
+            graphics::WHITE,
+        )?;
+        graphics::draw(
+            ctx,
+            &circle,
+            (ggez::nalgebra::Point2::new(self.pos_x, 380.0),),
+        )?;
+
+        graphics::present(ctx)?;
+        Ok(())
+    }
+}
+
+fn main() -> GameResult {
+    use ggez::conf::{WindowMode, WindowSetup};
+
+    let cb = ggez::ContextBuilder::new("Chip8", "ratschance")
+        .window_setup(WindowSetup {
+            title: "Chip8".to_owned(),
+            ..Default::default()
+        })
+        .window_mode(WindowMode {
+            width: C8_WIDTH as f32,
+            height: C8_HEIGHT as f32,
+            ..Default::default()
+        });
+    let (ctx, event_loop) = &mut cb.build()?;
+    let state = &mut MainState::new(ctx)?;
+    event::run(ctx, event_loop, state)
 }
