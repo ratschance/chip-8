9bc241ad09aa9caf61e0842cee262b347ce0687d
Conrad Ratschan <ratschance@gmail.com>
2020-05-25 22:32:45 -0500

    common: Add display routines
    Add display routines to get graphical output. Implement all remaining
    non-key-input related opcodes.

--- src/cpu.rs
+++ src/cpu.rs
@@ -1,7 +1,12 @@
+use rand::Rng;
+
+pub const C8_WIDTH: usize = 64;
+pub const C8_HEIGHT: usize = 32;
+
 pub struct Cpu {
     registers: Registers,
     memory: [u8; 4096],
-    display: [[bool; 32]; 64],
+    display: [[bool; C8_WIDTH]; C8_HEIGHT],
 }
 
 struct Registers {
@@ -21,7 +26,7 @@ impl Registers {
             i: 0,
             delay_timer: 0,
             sound_timer: 0,
-            pc: 0,
+            pc: 512,
             sp: 0,
             stack: [0; 16],
         }
@@ -33,7 +38,7 @@ impl Cpu {
         Cpu {
             registers: Registers::initialize(),
             memory: [0; 4096],
-            display: [[false; 32]; 64],
+            display: [[false; C8_WIDTH]; C8_HEIGHT],
         }
     }
 
@@ -70,7 +75,26 @@ impl Cpu {
         self.memory[..sprites.len()].copy_from_slice(&sprites[..])
     }
 
-    pub fn process_opcode(&mut self, opcode: u16) {
+    pub fn tick(&mut self) {
+        if self.registers.delay_timer > 0 {
+            self.registers.delay_timer -= 1;
+        }
+
+        if self.registers.sound_timer > 0 {
+            //TODO: Make sound
+            self.registers.sound_timer -= 1;
+        }
+
+        let pc = self.registers.pc as usize;
+        self.registers.pc += 2;
+        self.process_opcode((self.memory[pc] as u16) << 8 | self.memory[pc + 1] as u16);
+    }
+
+    pub fn view_display(&mut self) -> &[[bool; C8_WIDTH]; C8_HEIGHT] {
+        &self.display
+    }
+
+    fn process_opcode(&mut self, opcode: u16) {
         match opcode >> 12 {
             0x0 => {
                 // 0xxx
@@ -136,7 +160,7 @@ impl Cpu {
             0x7 => {
                 // 7xkk - ADD Vx, byte - Set Vx := Vx + kk
                 let x = get_nibble(2, opcode);
-                let val = self.registers.v[x] + (opcode & 0xff) as u8;
+                let val = self.registers.v[x].wrapping_add((opcode & 0xff) as u8);
                 self.registers.v[x] = val;
             }
             0x8 => {
@@ -166,15 +190,30 @@ impl Cpu {
             0xc => {
                 // Cxkk - RND Vx, byte - Set Vx := random byte AND kk
                 let x = get_nibble(2, opcode);
-                let val = opcode & 0xff;
-                //TODO: unimplemented
+                let kk = (opcode & 0xff) as u8;
+                let mut rng = rand::thread_rng();
+                self.registers.v[x] = rng.gen::<u8>() & kk;
             }
             0xd => {
                 // Dxyn - DRW Vx, Vy, nibble - Display n-byte sprite starting at memory location I at (Vx, Vy), set VF = collision
                 let x = get_nibble(2, opcode);
                 let y = get_nibble(1, opcode);
                 let n = get_nibble(0, opcode);
-                //TODO: unimplemented
+                for i in 0..n {
+                    let i_offset = self.registers.v[y] as usize + i;
+                    let sprite = self.memory[self.registers.i as usize + i];
+                    for j in 0..8 {
+                        let j_offset = self.registers.v[x] as usize + j;
+                        let pixel = (sprite >> (7 - j)) & 0x1;
+
+                        if j_offset < C8_WIDTH && i_offset < C8_HEIGHT && pixel == 0x1 {
+                            if self.display[i_offset][j_offset] {
+                                self.registers.v[0xF] = 1;
+                            }
+                            self.display[i_offset][j_offset] ^= true;
+                        }
+                    }
+                }
             }
             0xe => {
                 // Exoo - Key operations with Vx depending on the value of oo
@@ -259,7 +298,7 @@ impl Cpu {
                 self.registers.v[0xf] = self.registers.v[x] & 0x8;
                 self.registers.v[x] <<= 1;
             }
-            _ => panic!("Unhandled opcode: {}", op),
+            _ => panic!("Unhandled opcode_8: {}", op),
         }
     }
 
@@ -287,11 +326,20 @@ impl Cpu {
             }
             0x29 => {
                 // Fx29 - LD F, Vx - Set I := location of sprite for digit Vx
-                //TODO: unimplemented
+                self.registers.i = self.registers.v[x] as u16 * 5;
             }
             0x33 => {
                 // Fx33 - LD B, Vx - Store BCD representation of Vx in memory locations I, I+1, and I+2
-                //TODO: unimplemented
+                let mut val = self.registers.v[x];
+                let mut out = [0u8; 3]; // 0 - hundreds, 1 - tens, 2 - ones
+                for i in (0..2).rev() {
+                    if val != 0 {
+                        out[i] = (val % 10) as u8;
+                    }
+                    val /= 10;
+                }
+                let addr = self.registers.i as usize;
+                self.memory[addr..addr + 2].copy_from_slice(&out[..]);
             }
             0x55 => {
                 // Fx55 - LD [I], Vx - Store registers V0 through Vx, in memory starting at location I
@@ -305,7 +353,7 @@ impl Cpu {
                     self.registers.v[i] = self.memory[self.registers.i as usize + i];
                 }
             }
-            _ => panic!("Unhandled opcode: {}", op),
+            _ => panic!("Unhandled opcode_f: {}", op),
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,31 @@
 mod cpu;
 
 extern crate ggez;
+extern crate rand;
+
+use std::time::{Duration, Instant};
 
 use ggez::event;
 use ggez::graphics;
 use ggez::{Context, GameResult};
 
-const C8_WIDTH: u32 = 640;
-const C8_HEIGHT: u32 = 320;
+
+const PIXEL_SIZE: usize = 10;
+const SCREEN_WIDTH: usize = cpu::C8_WIDTH * PIXEL_SIZE;
+const SCREEN_HEIGHT: usize = cpu::C8_HEIGHT * PIXEL_SIZE;
+
+const MS_PER_UPDATE: u64 = (1000.0 / 60.0) as u64;
 
 struct MainState {
     cpu: cpu::Cpu,
+    last_update: Instant,
 }
 
 impl MainState {
     fn new(rom: &str) -> GameResult<MainState> {
         let mut s = MainState {
             cpu: cpu::Cpu::initialize(),
+            last_update: Instant::now(),
         };
         s.cpu.load_rom(rom);
         s.cpu.load_sprites();
@@ -27,21 +36,36 @@ impl MainState {
 
 impl event::EventHandler for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        if Instant::now() - self.last_update >= Duration::from_millis(MS_PER_UPDATE) {
+            self.last_update = Instant::now();
+            self.cpu.tick();
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.0, 0.0, 0.0, 0.0].into());
+        let display = self.cpu.view_display();
 
-        let circle = graphics::Mesh::new_circle(
-            ctx,
-            graphics::DrawMode::fill(),
-            ggez::nalgebra::Point2::new(0.0, 0.0),
-            100.0,
-            2.0,
-            graphics::WHITE,
-        )?;
-        graphics::draw(ctx, &circle, (ggez::nalgebra::Point2::new(0.0, 380.0),))?;
+        for i in 0..display.len() {
+            for j in 0..display[i].len() {
+                if display[i][j] {
+                    let rect_bounds = graphics::Rect::new_i32(
+                        (j * PIXEL_SIZE) as i32,
+                        (i * PIXEL_SIZE) as i32,
+                        PIXEL_SIZE as i32,
+                        PIXEL_SIZE as i32,
+                    );
+                    let filled_rect = graphics::Mesh::new_rectangle(
+                        ctx,
+                        graphics::DrawMode::fill(),
+                        rect_bounds,
+                        graphics::WHITE,
+                    )?;
+                    graphics::draw(ctx, &filled_rect, (ggez::nalgebra::Point2::new(0.0, 0.0),))?;
+                }
+            }
+        }
 
         graphics::present(ctx)?;
         Ok(())
@@ -58,8 +82,8 @@ fn main() -> GameResult {
             ..Default::default()
         })
         .window_mode(WindowMode {
-            width: C8_WIDTH as f32,
-            height: C8_HEIGHT as f32,
+            width: SCREEN_WIDTH as f32,
+            height: SCREEN_HEIGHT as f32,
             ..Default::default()
         });
     let (ctx, event_loop) = &mut cb.build()?;
